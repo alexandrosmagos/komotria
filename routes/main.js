f62451b617294aa59c72c160683ca5d47e681ce9
Stefanos Gregory <gregorystefanos@gmail.com>
2022-08-29 00:37:01 +0300

    Create login / register modal

--- routes/main.js
+++ routes/main.js
@@ -3,10 +3,11 @@ const router = require("express").Router();
 
 let titleD = "Demos";
 let h1D = "Demos";
+let mdl = "Register"; //TODO it will be login or register but use the same 1 modal
 
 // index page
 router.get("/", function(req, res) {
-	res.render("pages/index", {title: titleD, h1: h1D});
+	res.render("pages/index", {title: titleD, h1: h1D, modalTitle:mdl});
 });
 
 router.post("/api/sendMsg", function(req, res) {
