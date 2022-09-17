b784471ecbfafd128ebb97635737ffac3c57f786
Alexandros Magos <alexandrosmagos@hotmail.com>
2022-09-17 19:30:16 +0300

    chat card partial created, and dynamically loaded

--- routes/main.js
+++ routes/main.js
@@ -4,12 +4,23 @@ const router = require("express").Router();
 let titleD = "Demos";
 let h1D = "Demos";
 
+//Chat array with one object
+let chats = [
+	{
+		username: "Billy",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam. Odio aenean sed adipiscing diam donec adipiscing tristique risus. Tristique risus nec feugiat in fermentum posuere. Consequat id porta nibh venenatis cras sed felis. Purus sit amet volutpat consequat mauris nunc congue. At auctor urna nunc id cursus metus aliquam eleifend. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Et ultrices neque ornare aenean. Lacus sed turpis tincidunt id aliquet risus feugiat in."
+	},
+	// {
+	// 	username: "Bob"
+	// }
+];
+
 // index page
 router.get("/", function(req, res) {
 	if (req.isAuthenticated()) {
-		res.render("pages/index", {title: titleD, h1: h1D, user: req.user });
+		res.render("pages/index", {title: titleD, h1: h1D, user: req.user, chats });
 	} else {
-		res.render("pages/index", {title: titleD, h1: h1D});
+		res.render("pages/index", { title: titleD, h1: h1D, chats });
 	}
 	
 });
